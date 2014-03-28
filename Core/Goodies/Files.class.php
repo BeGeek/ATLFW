c2f43aadf1cd2473f2c2f4905516c2394b6ab743
Prieto Garcia Ivan Francisco <orishiku@ATL-V550>
2014-03-27 22:48:37 -0600

    First Lines of:      Files Class

---
+++ Core/Goodies/Files.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 
+ */
+class Files {
+	private $handle;
+	private $content;
+	private $contentArray = array();
+	
+	/**
+	 * 
+	 */
+	function __construct($root) {
+		
+	}
+}
+
+?>
